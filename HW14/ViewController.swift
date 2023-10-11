31d01bb3d80b4d7fb37b2a61e58d35894d0ce309
Gabitosz <gabriel.zdravkovici313@gmail.com>
2023-10-11 20:43:37 +0300

    Initial Commit

---
+++ HW14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW14
+//
+//  Created by Gabriel Zdravkovici on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
