894a87c7746d1267f1a1d58e87c7b5cb48109cdf
Gabitosz <gabriel.zdravkovici313@gmail.com>
2023-10-11 22:17:13 +0300

    add Nav Bar

--- HW14/Sources/App/SceneDelegate.swift
+++ HW14/Sources/App/SceneDelegate.swift
@@ -14,10 +14,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
+        let window = UIWindow(windowScene: windowScene)
         let viewController = AlbumScreenViewController()
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
+        self.window = window
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
